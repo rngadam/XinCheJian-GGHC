f448587d3e9a51fb21de90ab69fcffab67ffaeb9
Ricky Ng-Adam <rngadam@yahoo.com>
2011-05-02 02:28:50 +0800

    clean up formatting...

--- android/eclipse-workspace/FlashHostingTest/src/com/xinchejian/robot/flash/FlashHostingTest.java
+++ android/eclipse-workspace/FlashHostingTest/src/com/xinchejian/robot/flash/FlashHostingTest.java
@@ -1,9 +1,5 @@
 package com.xinchejian.robot.flash;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -17,71 +13,69 @@ import android.widget.Toast;
 public class FlashHostingTest extends Activity {
 	private JSInterface myJSInterface;
 	private WebView webView;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		webView = (WebView)findViewById(R.id.webView1);
-		
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		webView = (WebView) findViewById(R.id.webView1);
+
+		// needed configuration
 		webView.getSettings().setAllowFileAccess(true);
 		webView.getSettings().setPluginsEnabled(true);
 		webView.getSettings().setJavaScriptEnabled(true);
-		
-		myJSInterface = new JSInterface(webView);
-		final Context myApp = this; 
+
+		// only start calling Javascript when page fully loaded
 		webView.setWebViewClient(new WebViewClient() {
-		    @Override
-	       public void onPageFinished(WebView view, String url){
-		    	System.out.println("LOADED!");
-		    	webView.loadUrl("javascript:testEcho(\'Hello\')");
-	        }	
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				System.out.println("LOADED!");
+				webView.loadUrl("javascript:testEcho(\'Hello\')");
+			}
 		});
+
+		// enable "Javascript" alerts
+		myJSInterface = new JSInterface(webView);
+		final Context myApp = this;
 		
-		webView.setWebChromeClient(new WebChromeClient() {  
-		    @Override  
-		    public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result)  
-		    {  
-		        new AlertDialog.Builder(myApp)  
-		            .setTitle("javaScript dialog")  
-		            .setMessage(message)  
-		            .setPositiveButton(android.R.string.ok,  
-		                    new AlertDialog.OnClickListener()  
-		                    {  
-		                        public void onClick(DialogInterface dialog, int which)  
-		                        {  
-		                            result.confirm();  
-		                        }  
-		                    })  
-		            .setCancelable(false)  
-		            .create()  
-		            .show();  
-		  
-		        return true;  
-		    } 
-		    
-		});  
+		webView.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public boolean onJsAlert(WebView view, String url, String message,
+					final android.webkit.JsResult result) {
+				new AlertDialog.Builder(myApp)
+						.setTitle("javaScript dialog")
+						.setMessage(message)
+						.setPositiveButton(android.R.string.ok,
+								new AlertDialog.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int which) {
+										result.confirm();
+									}
+								}).setCancelable(false).create().show();
+
+				return true;
+			}
+
+		});
 		// register class containing methods to be exposed to JavaScript
-		webView.addJavascriptInterface(myJSInterface, "JSInterface"); 
-		//webView.loadDataWithBaseURL("fake://fake/fake", htmlPre+htmlCode+htmlPost, "text/html", "UTF-8", null);  
-		//webView.loadUrl("file:///sdcard/main.html");  
+		webView.addJavascriptInterface(myJSInterface, "JSInterface");
 		webView.loadUrl("file:///android_asset/main.html");
-		//webview.loadDataWithBaseURL( baseUrl, htmlText, "text/html", "UTF-8", null);
-		webView.loadUrl("javascript:testEcho(\'Hello\')");
-    }
-    
+	}
+
 	public class JSInterface {
 
 		private WebView mAppView;
-		public JSInterface  (WebView appView) {
-		        this.mAppView = appView;
-		    }
 
-		    public void doEchoTest(String echo){
-		        Toast toast = Toast.makeText(mAppView.getContext(), echo, Toast.LENGTH_SHORT);
-		        toast.show();
-		    }
+		public JSInterface(WebView appView) {
+			this.mAppView = appView;
+		}
+
+		public void doEchoTest(String echo) {
+			Toast toast = Toast.makeText(mAppView.getContext(), echo,
+					Toast.LENGTH_SHORT);
+			toast.show();
+		}
 	}
-	
-	
+
 }
